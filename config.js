39626b70fb5cccef1c8567aab736f32cd0b028bb
Ryd Wedding <ryd-wedding@example.com>
2026-03-23 20:51:03 +0700

    Total Premium Redesign with config.js system and Admin Dashboard

---
+++ config.js
@@ -0,0 +1,49 @@
+const weddingConfig = {
+    couple: {
+        groom: {
+            nickname: "Yusfi",
+            fullName: "Yusuf Supriyadi",
+            parents: "Putra dari Bpk. Selamet Riyadi & Ibu Munyati",
+            photo: "img/cowo1.jpg"
+        },
+        bride: {
+            nickname: "Marwah",
+            fullName: "Marwah Aulia",
+            parents: "Putri dari Bpk. Lorem Ipsum & Ibu Ipsum Loren",
+            photo: "img/cewe1.jpg"
+        }
+    },
+    event: {
+        date: "Sabtu, 21 Juni 2025",
+        timestamp: "2025-06-21T08:00:00", // For countdown
+        location: "Gedung BellaVista",
+        address: "Jl. Jatimakmur, Kec. Pondok Gede, Kota Bekasi",
+        mapsLink: "https://maps.app.goo.gl/uQsAwfTTj4KM1hBF6",
+        mapsEmbed: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.8449639038317!2d106.9250685!3d-6.284101499999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e698d448e95336f%3A0xf2186c7b64305948!2sGedung%20Serbaguna%20Bellavista!5e0!3m2!1sid!2sid!4v1770008678689!5m2!1sid!2sid",
+        akad: {
+            time: "08.00 - 10.00",
+            date: "Minggu, 29 Maret 2026"
+        },
+        resepsi: {
+            time: "11.00 - Selesai",
+            date: "Minggu, 29 Maret 2026"
+        }
+    },
+    gifts: [
+        {
+            bank: "BCA",
+            accountNumber: "1234567890",
+            owner: "Yusuf Supriyadi"
+        },
+        {
+            bank: "MANDIRI",
+            accountNumber: "0987654321",
+            owner: "Marwah Aulia"
+        }
+    ],
+    turutMengundang: [
+        "Keluarga Besar Bpk. Selamet Riyadi",
+        "Keluarga Besar Bpk. Lorem Ipsum",
+        "Sahabat & Rekan Kerja"
+    ]
+};
